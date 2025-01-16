bbd14a3efc3044cc14030a97f32f5503e9bd2609
SantiagoHrd <santiagohrd1@gmail.com>
2025-01-16 10:50:35 -0500

    Instantiate the class Job in the main program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.Write("");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2010;
+        job1._endYear = 2016;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Data analyst";
+        job2._startYear = 2016;
+        job2._endYear = 2025;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+
+
     }
 }
