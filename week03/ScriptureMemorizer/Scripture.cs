8e0bd4db7dff387b0b6af9473d9e283771644e18
SantiagoHrd <santiagohrd1@gmail.com>
2025-01-26 20:26:17 -0500

    Add the Clases for the program

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,26 @@
+public class Scripture
+{
+    private List<Word> _words;
+
+    private Reference _reference;
+
+    public Scripture(Reference reference, string text)
+    {
+        
+    }
+    
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool isCompletelyHidden()
+    {
+        return false;
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,30 @@
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool isHidden()
+    {
+        return true;
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
