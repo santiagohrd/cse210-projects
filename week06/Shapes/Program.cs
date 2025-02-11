022a94077d826b55a2136c8df6bc76dbca5c4c53
SantiagoHrd <santiagohrd1@gmail.com>
2025-02-11 15:31:42 -0500

    W6 activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 12);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Blue", 8, 10);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Black", 6);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} {shape}");
+            Console.WriteLine($"Area: {shape.GetArea()} \n");
+        }
     }
 }
